d77604c2b2899c58b589a590bb77a73f56d13f32
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-18 00:08:29 +0500

    Add reponsive design

--- component/Header.jsx
+++ component/Header.jsx
@@ -1,54 +1,43 @@
 import {
   Box,
   Button,
-  Container,
+  Collapse,
   Flex,
-  HStack,
   IconButton,
-  Modal,
-  ModalBody,
-  ModalContent,
   Stack,
+  useDisclosure,
 } from '@chakra-ui/react'
 import React from 'react'
 import Image from 'next/image'
 import img from '../public/mylogo.png'
 import NextLink from 'next/link'
-import { HamburgerIcon, MenuIcon, CloseIcon } from '@chakra-ui/icons'
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 
 const Header = () => {
   const [show, setShow] = React.useState(false)
+  const { isOpen, onToggle } = useDisclosure()
   const toggleMenu = () => setShow(!show)
   return (
-    <Flex w="100%" 
-     color="black"
-    //  justifyContent="space-around"
-    justify={["space-between","space-around","space-around"]}
-    wrap="wrap"
-    mb={8}
-    p={8}
+    <Flex
+      w="100%"
+      color="black"
+      justify={['space-between', 'space-around', 'space-around']}
+      wrap="wrap"
+      mb={8}
+      p={8}
     >
-      {/* <Container >
-        <Box display={['flex',]} justifyContent='space-between'> */}
-     <Box>
-
+      <Box>
         <Image src={img} width="65px" height="55px" alt="Segun Adebayo" />
-     </Box>
-      <Box display={['flex','flex','flex']} gap={48}>
-        
-          <IconButton
-             onClick={toggleMenu}
-            aria-label="Open Menu"
-            size="lg"
-            icon={show ? <CloseIcon /> : <HamburgerIcon />}
-           
-            display={{ base: "block", md: "none" }}
-          />
-        
       </Box>
-      {/* </Box>
-      </Container>
-       */}
+      <Box display={['flex', 'flex', 'flex']} gap={48}>
+        <IconButton
+          onClick={toggleMenu}
+          aria-label="Open Menu"
+          size="lg"
+          icon={show ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ base: 'block', md: 'none' }}
+        />
+      </Box>
 
       <Box
         fontWeight="800"
@@ -56,13 +45,12 @@ const Header = () => {
       >
         <Stack
           marginTop={['50px', '10px', '10px']}
-          marginLeft={['56px', '10px', '10px']}
+          marginLeft={['14px', '10px', '10px']}
           gap={8}
-          textAlign="center"
-          alignItems="center"
+          textAlign={['left', 'center', 'center']}
+          alignItems={['right', 'center', 'center']}
           direction={['column', 'row', 'row']}
-          >
-          
+        >
           <NextLink colorScheme="teal" href="/How it Works">
             How it Works
           </NextLink>
@@ -72,20 +60,22 @@ const Header = () => {
           <NextLink colorScheme="teal" href="/Marketplace">
             Marketplace
           </NextLink>
-          <NextLink colorScheme="teal" href="/Sign In" passHref>
-            Sign In
-          </NextLink>
-          <Button
-            bg="black"
-            color="white"
-            px="12"
-            variant="outline"
-            borderColor="black.500"
-            _hover={{ bg: 'white', color: 'black' }}
-          >
-            <NextLink href="/Marketplace">Get Started</NextLink>
-          </Button>
-          
+          <Box>
+            <NextLink colorScheme="teal" href="/Sign In" passHref>
+              Sign In
+            </NextLink>
+            <Button
+              bg="black"
+              color="white"
+              px="10"
+              variant="outline"
+              marginLeft="30px"
+              borderColor="black.500"
+              _hover={{ bg: 'white', color: 'black' }}
+            >
+              <NextLink href="/Marketplace">Get Started</NextLink>
+            </Button>
+          </Box>
         </Stack>
       </Box>
     </Flex>
